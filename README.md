9a9c5319526871ce7ad95d2a01ef8353c8702671
Diego Pereira Grassato <dgrassato@ciandt.com>
2017-04-05 10:50:05 -0300

    Symfony Bundle uses FOS Rest and provides Basic Api functionality

---
+++ README.md
@@ -0,0 +1,30 @@
+API Basic Bundle
+====================
+
+Symfony Bundle uses FOS Rest and provides Basic Api functionality
+
+## Features
+* BaseController: A foundation for your api controllers
+* Pagination
+* Query-Filtering: Filter Lists
+* Basic traits
+* Basic service manager
+* Basic structure for test (unit and functional) - uses PHPUnit 6
+
+  
+## Requirements
+* FOSRestBundle
+* FOSUserBundle
+* JMSSerializerBundle
+* DMSFilterBundle
+* Pipeline  
+* PagerFanta
+* Fractal
+
+## Installation
+
+### Download using composer:
+
+```bash
+composer require dgrassato/api-base-bundle "*"
+```  
---
+++ Security/Authentication/AuthenticationSuccessListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BaseBundle\Security\Authentication;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class AuthenticationSuccessListener
+ * @package BaseBundle\Security
+ */
+class AuthenticationSuccessListener
+{
+    /**
+     * @param \Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['user']= array(
+            'username' => $user->getUsername(),
+            'roles'    => $user->getRoles(),
+        );
+
+        $event->setData($data);
+    }
+}
